202095ed265f07c14536d157ed99923c9da82307
Shashank <prishankofficial@gmail.com>
2021-11-18 22:56:39 +0530

    version 2

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,8 +3,11 @@ import "./App.css";
 import * as api from "../api/solana-minter";
 
 export const App = () => {
+  const [user, setUser] = React.useState("");
+
   const onHandleConnect = async () => {
     let connected_user = await api.connectToWallet();
+    setUser(connected_user);
   };
   const onHandleDisconnect = () => {
     api.disconnectFromWallet();
@@ -13,7 +16,9 @@ export const App = () => {
   return (
     <>
       <h1>Solana NFT Minter</h1>
-      <button onClick={onHandleConnect}>Connect</button>
+      <button onClick={onHandleConnect}>
+        {user !== null ? "connected" : "connect"}
+      </button>
       <button onClick={onHandleDisconnect}>Disconnect</button>
     </>
   );
